b567f538424bcb800c0e0add009d33029c4066f2
vld31 <vlad.tomulescu317@gmail.com>
2025-10-26 21:18:04 +0100

    QR Code generator

---
+++ Draw_QR.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "u8x8.h"
+#include "u8g2.h"
+#include "QrCode/qrcodegen.hpp"
+
--- Pico_OLED.cpp
+++ Pico_OLED.cpp
@@ -3,10 +3,14 @@
 #include "hardware/i2c.h"
 #include "u8x8.h"
 #include "u8g2.h"
+#include "QrCode/qrcodegen.hpp"
 #define I2C_PORT  i2c0
 #define I2C_SDA   4
 #define I2C_SCL   5
 #define OLED_ADDR 0x3C 
+
+using qrcodegen::QrCode;
+using qrcodegen::QrSegment;
 
 static u8g2_t u8g2;
 
@@ -40,13 +44,46 @@ static uint8_t u8x8_byte_pico_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, vo
 static uint8_t u8x8_gpio_and_delay_pico(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr) {
     switch (msg) {
         case U8X8_MSG_GPIO_AND_DELAY_INIT: return 1;
-        case U8X8_MSG_DELAY_MILLI:         sleep_ms(arg_int); return 1;
-        case U8X8_MSG_DELAY_10MICRO:       sleep_us(10);      return 1;
-        case U8X8_MSG_DELAY_100NANO:       return 1;
-        case U8X8_MSG_GPIO_RESET:          return 1; 
-        default:                           return 1;
+        case U8X8_MSG_DELAY_MILLI:         
+            sleep_ms(arg_int); 
+            return 1;
+        case U8X8_MSG_DELAY_10MICRO:       
+            sleep_us(10);      
+            return 1;
+        case U8X8_MSG_DELAY_100NANO:      
+            return 1;
+        case U8X8_MSG_GPIO_RESET:          
+            return 1; 
+        default:                           
+            return 1;
     }
 }
+
+static void drawqr(const QrCode &qr){
+    u8g2_ClearBuffer(&u8g2);
+    u8g2_SetDrawColor(&u8g2, 1);          
+
+    const int size = qr.getSize();           
+    const int scale = 2;                      
+    const int offsetX = (128 - size * scale) / 2;  
+    const int offsetY = (64 - size * scale) / 2;
+
+    for (int y = 0; y < size; y++) {
+        for (int x = 0; x < size; x++) {
+            if (qr.getModule(x, y)) {         
+                u8g2_DrawBox(&u8g2, offsetX + x * scale, offsetY + y * scale, scale, scale);
+            }
+        }
+    }
+    u8g2_SendBuffer(&u8g2);
+}
+
+static void funcDrawQr(){
+    const char *text = "https://google.com";
+    const QrCode qr  = QrCode::encodeText(text, QrCode::Ecc::LOW);
+    drawqr(qr);
+}
+
 int main() {
     stdio_init_all();
     
@@ -61,11 +98,9 @@ int main() {
     u8g2_InitDisplay(&u8g2);
     u8g2_SetPowerSave(&u8g2, 0);
     
-    u8g2_ClearBuffer(&u8g2);
-    u8g2_SetFont(&u8g2, u8g2_font_6x10_tf); 
-    u8g2_DrawStr(&u8g2, 0, 12, "Hello World");
-    u8g2_SendBuffer(&u8g2);
+    funcDrawQr();
+    
     while (true) {
-        sleep_ms(1000);
+        tight_loop_contents();
     }
 }
